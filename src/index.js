4eda51be1cebda9e3d855437117d6d16eb83d835
Mukmin Abd Samad <mukminl4@gmail.com>
2020-08-17 17:05:45 +0800

    Exercise 1.2

--- src/index.js
+++ src/index.js
@@ -13,18 +13,18 @@ const Content = ({
 }) => {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
     </>
   );
 };
+
+const Part = ({ part, exercise }) => (
+  <p>
+    {part} {exercise}
+  </p>
+);
 
 const Total = ({ total }) => {
   return <p>Number of exercise {total}</p>;
